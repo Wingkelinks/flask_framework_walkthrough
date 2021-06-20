0557309e0f3bdf905470f13468ff446a7de1097c
Whitney Turner <whitters.t@gmail.com>
2021-06-20 12:20:22 +1000

    Added contact.html and routed templates

--- run.py
+++ run.py
@@ -7,6 +7,17 @@ app = Flask(__name__)   #create instance of this and store in a variable 'app'
 def index():
     return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+    
+
 if __name__ == "_main_":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
